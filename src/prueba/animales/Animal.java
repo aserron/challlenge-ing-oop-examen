42572b9283a1914f615fe9f725b1745a2e2c723a
Andres Serron <andres@oneseventech.com>
2025-03-21 10:25:14 -0300

    Examen De OOP - Practices
    Agregando Ejercicios:
    - 16. Clase Abstracta Animal:

---
+++ src/prueba/animales/Animal.java
@@ -0,0 +1,15 @@
+/**
+ * @description: Clase abstracta Animal
+ * @author: Andres Serron
+ * @version: 1.0
+ * @since: 2025-03-21
+ */
+package prueba.animales;
+
+public abstract class Animal {
+    /**
+     * Método abstracto para hacer sonido
+     * @return AnimalSound con el sonido del animal
+     */
+    public abstract AnimalSoundEnum hacerSonido();
+} 
---
+++ src/prueba/animales/AnimalSoundEnum.java
@@ -0,0 +1,24 @@
+package prueba.animales;
+
+public enum AnimalSoundEnum {
+    DOG("woof"),
+    CAT("meow"),
+    COW("moo"),
+    SHEEP("baa"),
+    CHICKEN("cluck"),
+    PIG("oink"),
+    DUCK("quack"),
+    HORSE("neigh"),
+    LION("roar"),
+    SNAKE("hiss");
+
+    private final String sound;
+
+    AnimalSoundEnum(String sound) {
+        this.sound = sound;
+    }
+
+    public String getSound() {
+        return sound;
+    }
+}
